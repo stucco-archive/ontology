879a7f6218f0cb9e383e248822133ee584363e45
euf <euf@ornl.gov>
2013-07-03 16:26:40 -0400

    path fix

--- validate.py
+++ validate.py
@@ -8,13 +8,13 @@ from jsonschema import validate
 schema_file = './graphson_schema.json'
 schema = json.load( codecs.open(schema_file, 'r', encoding='utf-8') )
 
-obj_file = './normal_test_graph.json'
+obj_file = './tests/normal_test_graph.json'
 obj = json.load( codecs.open(obj_file, 'r', encoding='utf-8') )
 
 # If no exception is raised by validate(), the instance is valid.
 validate(obj, schema)
 
-obj_file = './extended_test_graph.json'
+obj_file = './tests/extended_test_graph.json'
 obj = json.load( codecs.open(obj_file, 'r', encoding='utf-8') )
 
 # If no exception is raised by validate(), the instance is valid.
