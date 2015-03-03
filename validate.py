62fbc565467dae5c31c2814273099b353f900b49
euf <euf@ornl.gov>
2015-03-03 13:51:04 -0500

    update test script and test cases

--- validate.py
+++ validate.py
@@ -14,7 +14,25 @@ obj = json.load( codecs.open(obj_file, 'r', encoding='utf-8') )
 # If no exception is raised by validate(), the instance is valid.
 validate(obj, schema)
 
+# load extended format json file
 obj_file = './tests/extended_test_graph.json'
+obj = json.load( codecs.open(obj_file, 'r', encoding='utf-8') )
+
+# If no exception is raised by validate(), the instance is valid.
+validate(obj, schema)
+
+# load schema and json file 
+schema_file = './stucco_schema.json'
+schema = json.load( codecs.open(schema_file, 'r', encoding='utf-8') )
+
+obj_file = './tests/stucco_test_graph.json'
+obj = json.load( codecs.open(obj_file, 'r', encoding='utf-8') )
+
+# If no exception is raised by validate(), the instance is valid.
+validate(obj, schema)
+
+# load another json file
+obj_file = './tests/stucco_larger_test_graph.json'
 obj = json.load( codecs.open(obj_file, 'r', encoding='utf-8') )
 
 # If no exception is raised by validate(), the instance is valid.
